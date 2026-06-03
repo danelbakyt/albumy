09ef16257651ce98b49743857ce020b477a74d74
Danel <danel.bakyt@gmail.com>
2026-06-03 17:37:36 +0300

    fix Markup import

--- albumy/decorators.py
+++ albumy/decorators.py
@@ -7,7 +7,8 @@
 """
 from functools import wraps
 
-from flask import Markup, flash, url_for, redirect, abort
+from markupsafe import Markup
+from flask import flash, url_for, redirect, abort
 from flask_login import current_user
 
 
